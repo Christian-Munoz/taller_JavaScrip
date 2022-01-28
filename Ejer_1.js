f4667c6f898951d56dbfb59fe9b06d7bf686054f
Christian-Munoz <Cjmr777@gmail.com>
2022-01-28 17:26:54 -0500

    primer ejercicio taller

---
+++ Ejer_1.js
@@ -0,0 +1,25 @@
+/* Ciclos con FOR */
+// 1. El estudiante debe generar una salida igual a la de la imagen
+
+// Restricciones: 
+
+// A. debe usar ciclos
+// B. en caso de usar condiciones solo puede utilizar 1 if else
+// C.Solo esta habilitado para usar 2 console.log
+
+// Extra: Solucion con menos codigo lleva puntos extra
+
+ for (let i = "*"; i <= "*" ; i++) 
+ {console.log(`
+ ****************
+ *              *
+ *              *
+ *              *
+ *              *
+ *              *
+ *              *
+ *              *
+ *              *
+ *              *
+ ****************`)
+ }
